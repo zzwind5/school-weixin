482083e3028ea81417699bbf36495a10d03aaeff
zjie <zjie@aerohive.com>
2015-10-12 15:58:23 +0800

    ssl

--- src/test/java/com/wxapi/process/WxApiTest.java
+++ src/test/java/com/wxapi/process/WxApiTest.java
@@ -1,5 +1,15 @@
 package com.wxapi.process;
 
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.AllowAllHostnameVerifier;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +39,40 @@ public class WxApiTest {
 		System.out.println(url);
 		
 		RestTemplate restTemp = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
+		restTemp.setRequestFactory(new HttpComponentsClientHttpRequestFactory(createHttpClient()));
 		restTemp.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
 		WxAccessTokenVo tokenVo = restTemp.getForObject(url, WxAccessTokenVo.class);
 		System.out.println(tokenVo.getAccessToken());
 		System.out.println(tokenVo.getExpiresIn());
+	}
+	
+	private HttpClient createHttpClient() {
+        try {
+//            SSLContext sslContext =
+//                    SSLContexts.custom()
+//                               .loadTrustMaterial(KeyStore.getInstance(KeyStore.getDefaultType()), new HiveTrustAllStrategy())
+//                               .build();
+        	TrustManager[] tm = { new JEEWeiXinX509TrustManager() };
+			SSLContext sslContext = SSLContext.getInstance("SSL", "SunJSSE");
+			sslContext.init(null, tm, new java.security.SecureRandom());
+
+            return HttpClientBuilder.create()
+                                    .setSslcontext(sslContext)
+                                    .setHostnameVerifier(new AllowAllHostnameVerifier())
+                                    .build();
+        }
+        catch (Exception e) {
+            return HttpClientBuilder.create().build();
+        }
+    }
+	
+	class JEEWeiXinX509TrustManager implements X509TrustManager {
+		public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+		}
+		public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+		}
+		public X509Certificate[] getAcceptedIssuers() {
+			return null;
+		}
 	}
 }
